bbcf62f867d33f61cbb70c92a743868980f30f55
Clément <clement.reniers@etu.univ-lyon1.fr>
2024-11-06 16:05:27 +0100

    twig : add composer

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Internship Management</title>
+    </head>
+    <body>
+        <h1>Internship Management</h1>
+        <a href="internship.php">Internship</a>
+        <a href="student.php">Student</a>
+        <a href="company.php">Company</a>
+    </body>
+</html>
