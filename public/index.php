8831e0aa4b13266c595a93be012f5637206e863b
Clément <clement.reniers@etu.univ-lyon1.fr>
2024-11-07 08:43:13 +0100

    index: added mvc structure

--- public/index.php
+++ public/index.php
@@ -2,6 +2,14 @@
 
 require_once '../vendor/autoload.php';
 
-$loader = new \Twig\Loader\FilesystemLoader('../templates');
-$twig = new \Twig\Environment($loader);
-echo $twig->render('index.twig');
+if($_GET['page']){
+    $page = $_GET['page'];
+    // require_once '../src/'.$page.'.php';
+    $loader = new \Twig\Loader\FilesystemLoader('../templates');
+    $twig = new \Twig\Environment($loader);
+    echo $twig->render($page.'.html.twig', $data);
+} else {
+    $loader = new \Twig\Loader\FilesystemLoader('../templates');
+    $twig = new \Twig\Environment($loader);
+    echo $twig->render('index.html.twig');
+}
