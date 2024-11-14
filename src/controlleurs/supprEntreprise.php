d61f2f8b4ee7c89dbd055ab594963f7837f82256
Clément <clement.reniers@etu.univ-lyon1.fr>
2024-11-14 07:47:35 +0100

    entreprises : add supprEntreprise

---
+++ src/controlleurs/supprEntreprise.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'src/crud/Read.php';
+require_once 'src/crud/Delete.php';
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $entreprise = Crud\getEntrepriseById($id);
+    if($entreprise){
+        Crud\deleteEntreprise($entreprise);
+    } else {
+        header('Location: index.php?page=entreprises&delete=error');
+    }
+    header('Location: index.php?page=entreprises&delete=success');
+} else {
+    header('Location: index.php?page=entreprises');
+}
