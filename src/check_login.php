8c990a9962cd1c5fc166913d09d0ff4b8f08afc8
Persson-dev <sim16.prib@gmail.com>
2024-11-17 14:40:42 +0100

    login: cleanup

--- src/check_login.php
+++ src/check_login.php
@@ -1,11 +1,8 @@
 <?php
 require_once 'src/Login.php';
 
-if(!Login\isUserConnected()) {
-    if(isset($_GET['page'])){
-        header("Location: index.php?page=login&not_connected=1&callback=".$_GET['page']);
-    } else {
-        header("Location: index.php?page=login&not_connected=1");
-    }
-    exit();
+if (!Login\isUserConnected()) {
+    // en incluant ce fichier, la page est déjà correctement établie
+    header("Location: index.php?page=login&redirect=" . $_GET["page"]);
+    exit;
 }
--- src/controlleurs/login.php
+++ src/controlleurs/login.php
@@ -1,28 +1,24 @@
 <?php
-    if(isset($_POST['username']) && isset($_POST['password'])) {
-        require_once "src/Login.php";
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $result = Login\connectEtudiant($username, $password);
-        if($result == \Crud\CheckResult::Success) {
-            $callback = $_GET['callback'] ?? 'accueil';
-            header("Location: index.php?page=" . $callback);
-        } else {
-            if(isset($_GET['callback'])){
-                header("Location: index.php?page=login&error=1&callback=".$_GET['callback']);
-            } else {
-                header("Location: index.php?page=login&error=1");
-            }
-        }
+
+$data = [];
+$data['redirect'] = $_GET['redirect'] ?? ($_POST['redirect'] ?? null);
+
+$page_redirect = isset($_GET['redirect']) ? "index.php?page=" . $_GET['redirect'] : "index.php";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    require_once "src/Login.php";
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $result = Login\connectEtudiant($username, $password);
+
+    if ($result == \Crud\CheckResult::Success) {
+        header("Location: " . $page_redirect);
+        exit;
     } else {
-        require_once "src/crud/Read.php";
-        $callback = isset($_GET['callback']) ? ['callback' => $_GET['callback']] : 0;
-        $not_connected = isset($_GET['not_connected']) ? ['not_connected' => $_GET['not_connected']] : 0;
-        $error =  isset($_GET['error']) ? ['error' => $_GET['error']] : 0;
-        $data = [];
-        foreach ([$callback, $not_connected, $error] as $option) {
-            if($option != 0){
-                $data = array_merge($data, $option);
-            }
-        }
+        $data['login_error'] = true;
+        $data['username'] = $username;
     }
+} else {
+    $data['not_connected'] = true;
+}
