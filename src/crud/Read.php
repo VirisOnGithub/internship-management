fa3439f237a4d0d2d8eb1a0d866f19d87611cb21
Persson-dev <sim16.prib@gmail.com>
2024-12-04 16:01:55 +0100

    Read.php: doc

--- src/crud/Read.php
+++ src/crud/Read.php
@@ -2,14 +2,24 @@
 
 namespace Crud;
 
+/**
+ * @file Read.php
+ * Opérations de lecture de la base de données
+ */
+
 require_once('src/db/SqlOperations.php');
 require_once('src/db/ModeleFactory.php');
 require_once('src/modele/ClassesProf.php');
+require_once('src/crud/CheckResult.php');
 
 use function SqlOperations\getLines;
 use function SqlOperations\getLinesWhere;
 use function SqlOperations\getLinesLike;
 
+/**
+ * Récupère les Classes de la base de données
+ * @return Classe [ ] un tableau de classe
+ */
 function getClasses(): array
 {
 	$classes = [];
@@ -19,6 +29,10 @@ function getClasses(): array
 	return $classes;
 }
 
+/**
+ * Récupère les entreprises de la base de données
+ * @return Entreprise [ ] un tableau d'entreprises
+ */
 function getEntreprises(): array
 {
 	$entreprises = [];
@@ -37,6 +51,10 @@ function getEntreprises(): array
 	return $entreprises;
 }
 
+/**
+ * Récupère les étudiants de la base de données
+ * @return Etudiant [ ] un tableau d'étudiants
+ */
 function getEtudiants(): array
 {
 	$etudiants = [];
@@ -46,6 +64,10 @@ function getEtudiants(): array
 	return $etudiants;
 }
 
+/**
+ * Récupère les missions de la base de données
+ * @return Mission [ ] un tableau de missions
+ */
 function getMissions(): array
 {
 	$missions = [];
@@ -55,6 +77,10 @@ function getMissions(): array
 	return $missions;
 }
 
+/**
+ * Récupère les professeurs de la base de données
+ * @return Professeur [ ] un tableau de professeurs
+ */
 function getProfesseurs(): array
 {
 	$professeurs = [];
@@ -64,6 +90,10 @@ function getProfesseurs(): array
 	return $professeurs;
 }
 
+/**
+ * Récupère les spécialités de la base de données
+ * @return Specialite [ ] un tableau de spécialités
+ */
 function getSpecialites(): array
 {
 	$specialites = [];
@@ -73,6 +103,10 @@ function getSpecialites(): array
 	return $specialites;
 }
 
+/**
+ * Récupère les stages de la base de données
+ * @return Stage [ ] un tableau de stages
+ */
 function getStages(): array
 {
 	$stages = [];
@@ -82,6 +116,11 @@ function getStages(): array
 	return $stages;
 }
 
+/**
+ * Récupère une classe spécifique de la base de donnée
+ * @param $id le numéro de la classe
+ * @return Classe ou null si la classe n'a pas été trouvée
+ */
 function getClasseById(int $id): ?\Classe
 {
 	$classe = getLinesWhere("classe", ["num_classe" => $id]);
@@ -90,6 +129,11 @@ function getClasseById(int $id): ?\Classe
 	return \ModeleFactory\createClasseFromTable($classe[0]);
 }
 
+/**
+ * Récupère une entreprise spécifique de la base de donnée
+ * @param $id le numéro de l'entreprise
+ * @return Entreprise ou null si l'entreprise n'a pas été trouvée
+ */
 function getEntrepriseById(int $id): ?\Entreprise
 {
 	$entreprise = getLinesWhere("entreprise", ["num_entreprise" => $id]);
@@ -98,6 +142,11 @@ function getEntrepriseById(int $id): ?\Entreprise
 	return \ModeleFactory\createEntrepriseFromTable($entreprise[0]);
 }
 
+/**
+ * Récupère une entreprise spécifique de la base de donnée et recupère en plus les spécialités de cet entreprise.
+ * @param $id le numéro de l'entreprise
+ * @return Entreprise ou null si l'entreprise n'a pas été trouvée
+ */
 function getEntrepriseByIdWithSpecialites(int $id): ?\Entreprise
 {
 	$entreprise = getLinesWhere("entreprise", ["num_entreprise" => $id]);
@@ -111,6 +160,12 @@ function getEntrepriseByIdWithSpecialites(int $id): ?\Entreprise
 	}
 	return $entreprise;
 }
+
+/**
+ * Récupère un étudiant spécifique de la base de donnée
+ * @param $id le numéro de l'étudiant
+ * @return Etudiant ou null si l'étudiant n'a pas été trouvé
+ */
 function getEtudiantById(int $id): ?\Etudiant
 {
 	$etudiant = getLinesWhere("etudiant JOIN classe USING(num_classe)", ["num_etudiant" => $id]);
@@ -119,6 +174,11 @@ function getEtudiantById(int $id): ?\Etudiant
 	return \ModeleFactory\createEtudiantFromTable($etudiant[0]);
 }
 
+/**
+ * Récupère une mission spécifique de la base de donnée
+ * @param $id le numéro de la mission
+ * @return Mission ou null si la mission n'a pas été trouvée
+ */
 function getMissionById(int $id): ?\Mission
 {
 	$mission = getLinesWhere("mission JOIN stage USING(num_stage) JOIN entreprise USING(num_entreprise) JOIN professeur USING(num_prof) JOIN etudiant USING(num_etudiant) JOIN classe USING(num_classe)", ["num_mission" => $id]);
@@ -127,6 +187,11 @@ function getMissionById(int $id): ?\Mission
 	return \ModeleFactory\createMissionFromTable($mission[0]);
 }
 
+/**
+ * Récupère un professeur spécifique de la base de donnée
+ * @param $id le numéro du professeur
+ * @return Professeur ou null si le professeur n'a pas été trouvé
+ */
 function getProfesseurById(int $id): ?\Professeur
 {
 	$professeur = getLinesWhere("professeur", ["num_prof" => $id]);
@@ -135,6 +200,11 @@ function getProfesseurById(int $id): ?\Professeur
 	return \ModeleFactory\createProfesseurFromTable($professeur[0]);
 }
 
+/**
+ * Récupère une spécialité spécifique de la base de donnée
+ * @param $id le numéro de la spécialité
+ * @return Specialite ou null si la spécialité n'a pas été trouvée
+ */
 function getSpecialiteById(int $id): ?\Specialite
 {
 	$specialite = getLinesWhere("specialite", ["num_spec" => $id]);
@@ -143,6 +213,11 @@ function getSpecialiteById(int $id): ?\Specialite
 	return \ModeleFactory\createSpecialiteFromTable($specialite[0]);
 }
 
+/**
+ * Récupère un stage spécifique de la base de donnée
+ * @param $id le numéro du stage
+ * @return Stage ou null si le stage n'a pas été trouvé
+ */
 function getStageById(int $id): ?\Stage
 {
 	$stage = getLinesWhere("stage JOIN entreprise USING(num_entreprise) JOIN professeur USING(num_prof) JOIN etudiant USING(num_etudiant) JOIN classe USING(num_classe)", ["num_stage" => $id]);
@@ -151,15 +226,12 @@ function getStageById(int $id): ?\Stage
 	return \ModeleFactory\createStageFromTable($stage[0]);
 }
 
-function getEntrepriseSpecialites(\Entreprise $entreprise): array
-{
-	$specialites = [];
-	foreach (getLinesWhere("spec_entreprise JOIN specialite USING(num_spec)", ["num_entreprise" => $entreprise->getNumero()]) as $specialite_line) {
-		array_push($specialites, \ModeleFactory\createSpecialiteFromTable($specialite_line));
-	}
-	return $specialites;
-}
-
+/**
+ * Récupère les classes d'un professeur
+ * @pre le professeur doit exister dans la base de données
+ * @param int $prof_id le numéor du professeur
+ * @return ClassesProf les classe du professeur
+ */
 function getProfesseurClasses(int $prof_id): \ClassesProf
 {
 	$classes = [];
@@ -175,25 +247,13 @@ function getProfesseurClasses(int $prof_id): \ClassesProf
 	return new \ClassesProf(getProfesseurById($prof_id), $classe_principale, $classes);
 }
 
-function chercherEntreprise(string $nom): array
-{
-	$entreprises = [];
-	$spec_entreprises = getLines("spec_entreprise JOIN specialite USING(num_spec)");
-	foreach (getLinesLike("entreprise", ["raison_sociale" => "%" . $nom . "%"]) as $entreprise_line) {
-		$entreprise = \ModeleFactory\createEntrepriseFromTable($entreprise_line);
-		array_push($entreprises, $entreprise);
-
-		// on ajoute les spécialités aux entreprises
-		foreach ($spec_entreprises as $spec_entreprise) {
-			if ($spec_entreprise['num_entreprise'] == $entreprise->getNumero()) {
-				$entreprise->ajouterSpecialite(\ModeleFactory\createSpecialiteFromTable($spec_entreprise));
-			}
-		}
-	}
-	return $entreprises;
-}
-
-// recherche par sépcialité permissive (au moins un élément)
+/**
+ * Recherche une entreprise sur plusieurs critères
+ * @param string $nom le nom de l'entreprise cherchée
+ * @param string $ville le nom de la ville de l'entreprise cherché
+ * @param Specialite[] $specialites les spécialités de l'entreprise cherchée
+ * @return Entreprise [ ] un tableau d'entreprise (potentiellement vide)
+ */
 function chercherEntreprisesParCritères(string $nom, string $ville, array $specialites): array
 {
 	if (empty($nom) && empty($ville) && empty($specialites))
@@ -234,41 +294,32 @@ function chercherEntreprisesParCritères(string $nom, string $ville, array $spec
 	return $entreprises;
 }
 
-
-
-enum CheckResult
-{
-	case NotFound;
-	case WrongPassword;
-	case Success;
-}
-
 /**
  * Vérifie la validité du login et du mot de passe
- * @return array{etudiant: \Etudiant, result: CheckResult}
+ * @return array{etudiant: Etudiant, result: CheckResult}
  */
 function checkEtudiant(string $login, string $mdp): array
 {
 	$etu_ligne = getLinesWhere("etudiant JOIN classe USING(num_classe)", ["login_etudiant" => $login]);
 	if (empty($etu_ligne))
-		return ["etudiant" => null, "result" => CheckResult::NotFound];
+		return ["etudiant" => null, "result" => \CheckResult::NotFound];
 	$etu = \ModeleFactory\createEtudiantFromTable($etu_ligne[0]);
 	if (password_verify($mdp, $etu->getMdp()))
-		return ["etudiant" => $etu, "result" => CheckResult::Success];
-	return ["etudiant" => null, "result" => CheckResult::WrongPassword];
+		return ["etudiant" => $etu, "result" => \CheckResult::Success];
+	return ["etudiant" => null, "result" => \CheckResult::WrongPassword];
 }
 
 /**
  * Vérifie la validité du login et du mot de passe
- * @return array{professeur: \Professeur, result: CheckResult}
+ * @return array{professeur: Professeur, result: CheckResult}
  */
 function checkProfesseur(string $login, string $mdp): array
 {
 	$prof_ligne = getLinesWhere("professeur", ["login_prof" => $login]);
 	if (empty($prof_ligne))
-		return ["professeur" => null, "result" => CheckResult::NotFound];
+		return ["professeur" => null, "result" => \CheckResult::NotFound];
 	$prof = \ModeleFactory\createProfesseurFromTable($prof_ligne[0]);
 	if (password_verify($mdp, $prof->getMdp()))
-		return ["professeur" => $prof, "result" => CheckResult::Success];
-	return ["professeur" => null, "result" => CheckResult::WrongPassword];
+		return ["professeur" => $prof, "result" => \CheckResult::Success];
+	return ["professeur" => null, "result" => \CheckResult::WrongPassword];
 }
